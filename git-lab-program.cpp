42de4fa08ef253cee2029455c8e0908c358ea9a8
David Aguirre Martinez <da476518@ohio.edu>
2020-09-04 21:31:31 +0200

    git-lab-program.cpp

---
+++ git-lab-program.cpp
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: David Aguirre Martinez
+ *        Date: 09/04/2020
+ * Description: Lab 2 
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl; 
+    return 0;
+}// main
